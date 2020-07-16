ded15e0fb91b013067954d81e74ccf99f8cece16
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-16 21:59:29 +0200

    feat: add home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./Header/Header";
+import Home from "./Home";
 
 const App = () => (
   <div>
     <Header />
-    Start Here!
+    <Home />
   </div>
 );
 
