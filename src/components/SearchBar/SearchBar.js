c7a3a9ef7935d39b080e9d8aea307a951a6eebc1
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-16 21:58:52 +0200

    feat: create searchbar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledSearchBar, StyledSearchBarContent } from "./StyledSearchBar";
+
+const SearchBar = () => {
+  return <div>SearchBar</div>;
+};
+
+export default SearchBar;
