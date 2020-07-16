d90fc75742d71184151c8d2947d5982b29702d40
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-16 21:58:13 +0200

    feat: create load more button

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledLoadMoreBtn } from "./StyledLoadMoreBtn";
+
+const LoadMoreBtn = () => {
+  return <div>LoadMoreBtn</div>;
+};
+
+export default LoadMoreBtn;
